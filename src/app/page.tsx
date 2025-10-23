417c4ad97560145680680e7113b147f1165cf6d8
MatheusZamo <19matheushenrique03@gmail.com>
2025-10-22 22:48:15 -0300

    Created the layout folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/features/common/Footer";
-import Header from "@/features/common/Header";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 
 
 
