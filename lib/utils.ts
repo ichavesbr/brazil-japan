502b0d83c1927793bacd962c34b2a7ce6bbf557d
MatheusZamo <19matheushenrique03@gmail.com>
2025-10-25 20:36:20 -0300

    removing duplicate folders, app and lib

--- lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: (string | undefined | null | boolean | Record<string, boolean>)[]): string {
-  return twMerge(clsx(inputs))
-}
