d3c8a264d856d07dcfe1314e65dcd3092dcec196
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-11-27 03:14:39 +0900

    create product-fabricator schema interface

---
+++ src/features/product-fabricator/schemas/product.fabricator.schema.ts
@@ -0,0 +1,25 @@
+export interface ProductFabricator {
+  id: string
+  description: string
+  status_id: string
+  created_at: string
+  updated_at?: string | null
+}
+
+export interface ProductCategorFabricator {
+  description: string
+  status_id: string
+}
+
+export interface ProductCategoryFabricator {
+  description: string
+  status_id: string
+}
+
+export interface ProductCategoryFabricator {
+  id: string
+  description: string
+  status_id: string
+  created_at: string
+  updated_at?: string | null
+}
