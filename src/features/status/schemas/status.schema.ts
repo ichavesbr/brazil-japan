7056332a528082d0d7bf00732d0143cea01bc8fd
MatheusZamo <19matheushenrique03@gmail.com>
2025-11-28 23:22:16 -0300

    update status schema interfaces

--- src/features/status/schemas/status.schema.ts
+++ src/features/status/schemas/status.schema.ts
@@ -1,6 +1,15 @@
-interface Status {
+export interface Status {
   id: number
   description: string
   created_at: Date
+  updated_at?: Date
+}
+export interface StatusUpdate {
+  description?: string
   updated_at: Date
+}
+export interface StatusSearch {
+  id?: number
+  description?: string
+  created_at?: Date
 }
