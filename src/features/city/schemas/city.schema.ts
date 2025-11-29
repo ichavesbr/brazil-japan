1bbd0bc4c0fd298645258f02e04f6490e5d1e40a
MatheusZamo <19matheushenrique03@gmail.com>
2025-11-28 22:55:13 -0300

    update city schema interfaces

--- src/features/city/schemas/city.schema.ts
+++ src/features/city/schemas/city.schema.ts
@@ -1,6 +1,17 @@
-interface City {
+export interface City {
   id: number
+  state_id: number
   description: string
   created_at: Date
+  updated_at?: Date
+}
+export interface CityUpdate {
+  description?: string
   updated_at: Date
+}
+export interface CitySearch {
+  id?: number
+  state_id?: number
+  description?: string
+  created_at?: Date
 }
