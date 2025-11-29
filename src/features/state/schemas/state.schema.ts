627c9c7a96dc01417d8c679e4810efab73a531d0
MatheusZamo <19matheushenrique03@gmail.com>
2025-11-28 23:14:23 -0300

    update state schema interfaces

--- src/features/state/schemas/state.schema.ts
+++ src/features/state/schemas/state.schema.ts
@@ -1,6 +1,15 @@
-interface State {
+export interface State {
   id: number
   description: string
   created_at: Date
+  updated_at?: Date
+}
+export interface StateUpdate {
+  description?: string
   updated_at: Date
+}
+export interface StateSearch {
+  id?: number
+  description?: string
+  created_at?: Date
 }
