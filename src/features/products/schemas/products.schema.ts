e5f398efeb506e7031f1088bc3c4e9f316f5109c
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-01 01:20:12 +0900

    update product schema interface

--- src/features/products/schemas/products.schema.ts
+++ src/features/products/schemas/products.schema.ts
@@ -1,13 +1,35 @@
 export interface Product {
-  id: string
-  name: string
+  id: number
+  status_id: number
+  category_id: number
+  type_unit_id: number
+  fabricator_id: number
   description: string
-  price: number
-  stock: number
-  category: string
-  imageUrl: string
-  createdAt: string
-  isFeatured: boolean
-  isPromotion: boolean
-  isFavorite: boolean
+  quantity_stock: number
+  price_unitary: number
+  created_at: Date
+  updated_at?: Date
+}
+
+export interface ProductInputData {
+  description: string
+  quantity_stock: number
+  price_unitary: number
+}
+export interface ProductUpdateData {
+  description?: string
+  quantity_stock?: number
+  price_unitary?: number
+  updated_at?: Date
+}
+
+export interface ProductSearchData {
+  id?: number
+  category_id?: number
+  type_unit_id?: number
+  fabricator_id?: number
+  description?: string
+  quantity_stock?: number
+  price_unitary?: number
+  created_at?: Date
 }
