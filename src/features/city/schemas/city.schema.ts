f743e6826d04d481e2100cb4bff719a64548a461
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-07 13:49:54 +0900

    altera schema city #27

--- src/features/city/schemas/city.schema.ts
+++ src/features/city/schemas/city.schema.ts
@@ -5,13 +5,12 @@ export interface City {
   created_at: Date
   updated_at?: Date
 }
-export interface CityUpdate {
-  description?: string
-  updated_at: Date
+export interface CityInput {
+  state_id: number
+  description: string
 }
 export interface CitySearch {
   id?: number
   state_id?: number
   description?: string
-  created_at?: Date
 }
