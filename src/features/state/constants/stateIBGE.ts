e6479290e18699841a1ff13b1c5bf34068e549e3
Igor Chaves Donega <ig.chavesbr@gmail.com>
2025-12-07 14:31:20 +0900

    conserta nome da pasta do state

---
+++ src/features/state/constants/stateIBGE.ts
@@ -0,0 +1,33 @@
+import { type State } from "../schemas/state.schema"
+
+const CREATED_AT = new Date("2025-12-07")
+
+export const STATES_IBGE: State[] = [
+  { id: 12, description: "Acre", created_at: CREATED_AT },
+  { id: 27, description: "Alagoas", created_at: CREATED_AT },
+  { id: 16, description: "Amapá", created_at: CREATED_AT },
+  { id: 13, description: "Amazonas", created_at: CREATED_AT },
+  { id: 29, description: "Bahia", created_at: CREATED_AT },
+  { id: 23, description: "Ceará", created_at: CREATED_AT },
+  { id: 53, description: "Distrito Federal", created_at: CREATED_AT },
+  { id: 32, description: "Espírito Santo", created_at: CREATED_AT },
+  { id: 52, description: "Goiás", created_at: CREATED_AT },
+  { id: 21, description: "Maranhão", created_at: CREATED_AT },
+  { id: 51, description: "Mato Grosso", created_at: CREATED_AT },
+  { id: 50, description: "Mato Grosso do Sul", created_at: CREATED_AT },
+  { id: 31, description: "Minas Gerais", created_at: CREATED_AT },
+  { id: 15, description: "Pará", created_at: CREATED_AT },
+  { id: 25, description: "Paraíba", created_at: CREATED_AT },
+  { id: 41, description: "Paraná", created_at: CREATED_AT },
+  { id: 26, description: "Pernambuco", created_at: CREATED_AT },
+  { id: 22, description: "Piauí", created_at: CREATED_AT },
+  { id: 24, description: "Rio Grande do Norte", created_at: CREATED_AT },
+  { id: 43, description: "Rio Grande do Sul", created_at: CREATED_AT },
+  { id: 33, description: "Rio de Janeiro", created_at: CREATED_AT },
+  { id: 11, description: "Rondônia", created_at: CREATED_AT },
+  { id: 14, description: "Roraima", created_at: CREATED_AT },
+  { id: 42, description: "Santa Catarina", created_at: CREATED_AT },
+  { id: 35, description: "São Paulo", created_at: CREATED_AT },
+  { id: 28, description: "Sergipe", created_at: CREATED_AT },
+  { id: 17, description: "Tocantins", created_at: CREATED_AT },
+]
